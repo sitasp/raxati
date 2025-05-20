669e988b8bb2d9797b85de1a93ffac45a34faf3e
satish.patra <psatishkumar720@gmail.com>
2025-05-20 23:45:56 +0530

    Update Event.java

--- src/main/java/org/sage/object/domain/Event.java
+++ src/main/java/org/sage/object/domain/Event.java
@@ -2,7 +2,6 @@ package org.sage.object.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
